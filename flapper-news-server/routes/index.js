c78cfbbd9e4ce1e93136c39bf262952851696300
Asim K T <aasimkt@gmail.com>
2016-02-09 13:09:47 +0530

    Finishing Off With Comments

--- flapper-news-server/routes/index.js
+++ flapper-news-server/routes/index.js
@@ -17,6 +17,19 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
+// Not tested
+router.param('comment', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -30,10 +43,13 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
-});
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
 
+    res.json(post);
+  });
+});
 router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
@@ -46,6 +62,31 @@ router.post('/posts', function(req, res, next) {
 
 router.put('/posts/:post/upvote', function(req, res, next) {
   req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+// Functionality not tested
+router.post('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.post.comment.upvote(function(err, post){
     if (err) { return next(err); }
 
     res.json(post);
